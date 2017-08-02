cc56343f463489b68f86885f7b34f7814663d828
Kevin Fang <kfang@commschool.org>
2017-08-02 11:09:25 -0400

    Added better command for single command client starting

--- clients/startBackend.sh
+++ clients/startBackend.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-results=$(arv)
-
-if [[ $results =~ ^[ARVADOS_API_HOST] ]]
-then
-	cd .. && ./setup.sh && cd clients && cd tile-searcher-backend && npm i && npm start
-else 
-	echo "Set tokens and retry";
-fi
-
+cd tile-searcher-backend && npm i && npm start
